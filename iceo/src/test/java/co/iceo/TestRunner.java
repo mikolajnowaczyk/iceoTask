d4350156fe0eb00dc5cd7312db840459324cc24e
Mikolaj <mikolaj35@wp.pl>
2023-05-30 20:29:49 +0200

    Runner options update

--- iceo/src/test/java/co/iceo/TestRunner.java
+++ iceo/src/test/java/co/iceo/TestRunner.java
@@ -1,11 +1,13 @@
 package co.iceo;
 
 import org.junit.runner.RunWith;
-import io.cucumber.junit.CucumberOptions;
+
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(glue = { "co.iceo" }, plugin = { "pretty", "html:target/cucumber-reports/Report.html" })
+@CucumberOptions(features = "src/test/java", glue = { "co.iceo" }, plugin = { "pretty",
+	"html:target/cucumber-reports/Report.html" })
 public class TestRunner {
 
 }
